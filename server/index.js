1e7a73e854b2fdeed75787f915e3914f2809986c
Brian Kim <briaansjkim@gmail.com>
2019-10-23 22:41:26 -0400

    proxy server properly set up

--- server/index.js
+++ server/index.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 const path = require('path');
-const morgan = require('morgan');
-const proxy = require('express-http-proxy');
+const cors = require('cors');
+// const proxy = require('http-proxy-middleware');
 const port = process.env.PORT || 5678;
-
-app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname, './public')));
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', proxy('http://localhost:1258'));
-// app.use('/', proxy('http://localhost:3000'));
-
-// app.get('/', (req, res) => {
-//   res.send('Hello')
-// })
 
 app.listen(port, () => {
   console.log('Server listening on port: ', port)
