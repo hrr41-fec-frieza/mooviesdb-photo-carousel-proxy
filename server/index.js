1c485e932c8e837e8a885b6a9ceeaf804477914c
Brian Kim <briaansjkim@gmail.com>
2019-10-24 13:23:54 -0400

    renders 3 services, waiting on 4th

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
-// const proxy = require('http-proxy-middleware');
 const port = process.env.PORT || 5678;
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../public')));
